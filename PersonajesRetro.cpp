ea27bb2fcfafd49a67f542956cf06858549e0a67
Alan Echeveste <alan.echeveste2963@alumnos.udg.mx>
2020-09-18 11:25:31 -0500

    Funciona captura de enteros y mostrar enteros con suma y promedio

--- PersonajesRetro.cpp
+++ PersonajesRetro.cpp
@@ -2,6 +2,27 @@
 
 using namespace std;
 
+void capturarEnteros(){
+    int enteros[5], suma=0;
+    float promedio;
+
+    for(int i=0;i<5;i++)
+    {
+        cin>>enteros[i];
+    }
+    cout<<endl;
+    for(int i=0;i<5;i++)
+    {
+        cout<<enteros[i]<<endl;
+    }
+    for(int i=0;i<5;i++)
+    {
+        suma += enteros[i];
+    }
+    cout<<"Suma: "<<suma<<endl;
+    cout<<"Promedio: "<<suma/5<<endl;
+}
+
 int main(){
     char op;
 
@@ -15,5 +36,25 @@ int main(){
 
         fflush(stdin);
 
-    }while (op != 0);
+        switch(op)
+        {
+        case '1':
+            cout<<"Ingrese 5 numeros enteros"<<endl;
+            capturarEnteros();
+            break;
+
+        case '2':
+            break;
+
+        case '3':
+            break;
+
+        default:
+            break;
+        }
+
+    }while (op != '0');
+
+    return 0;
 }
+
